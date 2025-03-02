54dda8ef5ed737438a84a2cd4898530d2e72365a
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-03-02 12:57:27 +0100

    MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.46.1, and conda-forge-pinning 2025.03.02.02.22.55

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,4 @@
-cmake -G "Ninja Multi-Config" -B build --preset conda_raspalib -DCMAKE_INSTALL_PREFIX=%PREFIX%
+cmake ${CMAKE_ARGS} -G "Ninja Multi-Config" -B build --preset conda_raspalib -DCMAKE_INSTALL_PREFIX=%PREFIX%
 if errorlevel 1 exit 1
 ninja -C build install
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-cmake -B build --preset conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+  cmake -B build --preset conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
+else
+  cmake ${CMAKE_ARGS} -B build --preset conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
+fi
 ninja -C build install
