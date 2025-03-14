819ab574274055a26e4dbb90909e127dc37f4878
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2025-03-14 16:16:29 +0100

    MNT: Re-rendered with conda-build 25.1.2, conda-smithy 3.47.0, and conda-forge-pinning 2025.03.14.13.23.22

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR:-}" != "" ]]; then
-  if [[ "${target_platform}" == linux-64 ]]; then
-    cmake -B build --preset linux_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
+
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
+  if [[ "${target_platform}" == linux* ]]; then
+    cmake -B build --preset=linux_conda_raspalib
   elif [[ "${target_platform}" == osx-* ]]; then
-    cmake ${CMAKE_ARGS} -B build --preset mac_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
+    cmake -B build --preset=mac_conda_raspalib
   fi
 else
-  if [[ "${target_platform}" == linux-ppc64le ]]; then
-    cmake ${CMAKE_ARGS} -B build --preset linux_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
-  elif [[ "${target_platform}" == linux-aarch64 ]]; then
-    cmake ${CMAKE_ARGS} -B build --preset linux_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX}
+  if [[ "${target_platform}" == linux* ]]; then
+    cmake -B build --preset=linux_conda_raspalib ${CMAKE_ARGS}
   elif  [[ "${target_platform}" == osx-* ]]; then
-    cmake ${CMAKE_ARGS} -B build --preset mac_conda_raspalib -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_OSX_ARCHITECTURES="arm64"
+    cmake -B build --preset=mac_conda_raspalib ${CMAKE_ARGS}
   fi
 fi
-ninja -C build install -v
 
+ninja -C build install -v
