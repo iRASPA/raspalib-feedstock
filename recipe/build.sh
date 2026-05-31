422d5eb225c58c359c6699ea9f830137eaf0a436
David Dubbeldam <david.dubbeldam@kpnmail.nl>
2026-05-31 10:31:17 +0200

    chore: updated v3.0.30 (#68)
    * MNT: Re-rendered with conda-smithy 2026.5.29 and conda-forge-pinning 2026.05.30.09.17.10
    
    Other tools:
    - conda-build 26.5.0
    - rattler-build 0.65.0
    - rattler-build-conda-compat 1.4.14
    
    * chore: updated v3.0.30
    
    * MNT: Re-rendered with conda-smithy 2026.5.29 and conda-forge-pinning 2026.05.30.13.41.53
    
    Other tools:
    - conda-build 26.5.0
    - rattler-build 0.65.0
    - rattler-build-conda-compat 1.4.14
    
    ---------
    
    Co-authored-by: David Dubbeldam <d.dubbeldam@uva.nl>

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,9 +2,9 @@
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" ]]; then
   if [[ "${target_platform}" == linux* ]]; then
-    cmake -B build --preset=linux_conda_raspalib -DCMAKE_POLICY_VERSION_MINIMUM=3.32
+    cmake -B build --preset=linux_conda_raspalib ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   elif [[ "${target_platform}" == osx-* ]]; then
-    cmake -B build --preset=mac_conda_raspalib -DCMAKE_POLICY_VERSION_MINIMUM=3.32
+    cmake -B build --preset=mac_conda_raspalib ${CMAKE_ARGS} -DCMAKE_POLICY_VERSION_MINIMUM=3.32
   fi
 else
   if [[ "${target_platform}" == linux* ]]; then
